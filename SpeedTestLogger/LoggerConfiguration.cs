e7c1292b7197cb05f75bf5af845deae2b9fb9847
joingestubbe <jstubbe@gmail.com>
2018-11-03 13:50:46 +0100

    My first speedTestLogger

---
+++ SpeedTestLogger/LoggerConfiguration.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace SpeedTestLogger
+{
+    public class LoggerConfiguration
+    {
+        public readonly RegionInfo LoggerLocation;
+        public readonly string UserId;
+        public readonly int LoggerId;
+
+        public LoggerConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            var countryCode = configuration["loggerLocationCountryCode"];
+            UserId = configuration["userId"];
+            LoggerId = Int32.Parse(configuration["loggerId"]);
+
+            LoggerLocation = new RegionInfo(countryCode);
+
+            System.Console.WriteLine("logger located in {0}", LoggerLocation.EnglishName);
+
+        }
+    }
+}
---
+++ SpeedTestLogger/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using SpeedTest;
+using SpeedTest.Models;
+using System;
+using System.Globalization;
+using System.Linq;
+
+using SpeedTestLogger.Models;
+
+namespace SpeedTestLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //     Console.WriteLine("Hello World!");
+           System.Console.WriteLine("Hello SpeedTestLogger");
+        //     var client = new SpeedTestClient();
+        //     var settings = client.GetSettings();
+
+        //     System.Console.WriteLine("finding test server");
+            var config = new LoggerConfiguration();
+
+            // var location = new RegionInfo("nb-NO");
+             var runner = new SpeedTestRunner ( config.LoggerLocation );
+             runner.RunSpeedTest();
+
+             var testData = runner.RunSpeedTest();
+             var results = new TestResult
+             {
+                 SessionId = new Guid(),
+                 User = config.UserId,
+                 Device = config.LoggerId,
+                 Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                 Data = testData
+
+             };
+             
+        //     var tenLocalServers = settings.Servers 
+        //         .Where (s=> s.Country.Equals(location.EnglishName
+        //     )).Take(10);
+
+        //     var serversOrdersByLatency = tenLocalServers
+        //         .Select ( s => 
+        //         {
+        //             s.Latency = client.TestServerLatency(s);
+        //              return s;
+        //         })
+        //         .OrderBy(s => s.Latency);
+                
+
+        // var server = serversOrdersByLatency.First();
+
+        // Console.WriteLine("Testing download speed");
+        // var downloadSpeed = client.TestDownloadSpeed(server, settings.Download.ThreadsPerUrl);
+        // Console.WriteLine("Download speed was: {0} Mbps", Math.Round(downloadSpeed / 1024, 2));
+
+        // Console.WriteLine("Testing upload speed");
+        // var uploadSpeed = client.TestUploadSpeed(server, settings.Upload.ThreadsPerUrl);
+        // Console.WriteLine("Upload speed was: {0} Mbps", Math.Round(uploadSpeed / 1024, 2))// ;
+
+        }
+    }
+    
+}
